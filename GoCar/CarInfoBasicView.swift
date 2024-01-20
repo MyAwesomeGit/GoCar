9645d69ba26223d8a1102de78f8cb41b0e210c62
Vasily Shuklin <b88bb@yandex.ru>
2024-01-20 18:00:03 +0500

    Updated  CarInfoBasicView
    Signed-off-by: Vasily Shuklin <b88bb@yandex.ru>

--- GoCar/CarInfoBasicView.swift
+++ GoCar/CarInfoBasicView.swift
@@ -1,8 +1,34 @@
-//
-//  CarInfoBasicView.swift
-//  GoCar
-//
-//  Created by Vasily Shuklin on 20.01.2024.
-//
 
-import Foundation
+import SwiftUI
+
+struct CarInfoBasicView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0)
+        {
+            HStack {
+                Text("Tesla Model S")
+                Spacer()
+                Image(systemName: "info.circle")
+                    .font(.system(size: 28))
+                    .offset(y: -2)
+            }
+            VStack(alignment: .leading, spacing: 4) {
+                Text("$80/hr")
+                    .foregroundColor(.gray)
+                HStack {
+                    ForEach(0..<5) { _ in
+                        Image(systemName: "star.fill")
+                            .font(.system(size: 15))
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct CarInfoBasicView_Previews: PreviewProvider {
+    static var previews: some View {
+        CarInfoBasicView()
+            .previewLayout(.fixed(width: 400, height: 100))
+    }
+}
